0f85e6758d86e73d275fbe733da941b8bbd4962d
ankur0102 <ankur.s0102@gmail.com>
2020-09-14 22:43:42 +0530

    change application properties

--- src/applicationProperties.js
+++ src/applicationProperties.js
@@ -1,7 +1,7 @@
-export const POST_MESSAGE = 'https://7e338e7a486d.ngrok.io/insertMessage';
-export const GET_MESSAGE = 'https://7e338e7a486d.ngrok.io/retrieveMessage';
-export const CHAT_USERS = 'https://7e338e7a486d.ngrok.io/retrieveUsers';
-export const WEB_SOCKET = 'wss://632372b785b8.ngrok.io/';
-export const AUTHENTICATE_USER = 'https://7e338e7a486d.ngrok.io/authenticateUser';
-export const INSERT_USER = 'https://7e338e7a486d.ngrok.io/insertUser';
-export const INSERT_CHAT = 'https://7e338e7a486d.ngrok.io/insertChat';
+export const POST_MESSAGE = 'https://thunder-chat-backend.herokuapp.com/insertMessage';
+export const GET_MESSAGE = 'https://thunder-chat-backend.herokuapp.com/retrieveMessage';
+export const CHAT_USERS = 'https://thunder-chat-backend.herokuapp.com/retrieveUsers';
+export const WEB_SOCKET = 'wss://thunder-socket.herokuapp.com/';
+export const AUTHENTICATE_USER = 'https://thunder-chat-backend.herokuapp.com/authenticateUser';
+export const INSERT_USER = 'https://thunder-chat-backend.herokuapp.com/insertUser';
+export const INSERT_CHAT = 'https://thunder-chat-backend.herokuapp.com/insertChat';
